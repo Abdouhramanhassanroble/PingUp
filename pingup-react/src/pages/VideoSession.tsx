b39b7bf488a8c089d3e11fe57ee71fde0ebc2123
Kudo <103179860+Abdouhramanhassanroble@users.noreply.github.com>
2025-11-14 20:34:50 +0100

    correction du VideoSession.tsx

--- pingup-react/src/pages/VideoSession.tsx
+++ pingup-react/src/pages/VideoSession.tsx
@@ -7,8 +7,7 @@ import JitsiRoom from '../Jitsi/JitsiRoom';
 import Navbar from '../components/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
-  faVideo, faVideoSlash, faMicrophone, faMicrophoneSlash,
-  faDesktop, faComments, faSignOutAlt, faUsers, faClock
+  faVideo, faVideoSlash, faSignOutAlt, faUsers, faClock
 } from '@fortawesome/free-solid-svg-icons';
 import './VideoSession.css';
 
